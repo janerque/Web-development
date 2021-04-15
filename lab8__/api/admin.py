a64cf0c7c815556b9ae07fff4d0e11fb03d90f91
janerque <64560065+janerque@users.noreply.github.com>
2021-04-15 22:12:06 +0600

    deleae

--- lab8__/api/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-
-# Register your models here.
-from api.models import Product
-from api.models import Category
-
-admin.site.register(Product)
-admin.site.register(Category)
--- lab8__/env/Scripts/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!c:\users\legion\desktop\lab8\env\scripts\python.exe
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()
